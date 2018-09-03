aaefd76175cff62bbe9be160e7ee945658ec5c0b
icecube <ice@cube.io>
2018-09-03 18:35:39 +0100

    Travis & Pre Deploy Update

--- pre-deploy.sh
+++ pre-deploy.sh
@@ -3,39 +3,39 @@
 OUTDIR=$TRAVIS_BUILD_DIR/out/$TRAVIS_PULL_REQUEST/$TRAVIS_JOB_NUMBER-$HOST
 mkdir -p $OUTDIR/bin
 
-echo $TRAVIS_BUILD_DIR
-
 ARCHIVE_CMD="zip"
+ARCHIVE_NAME=""
 
 if [[ $HOST = "i686-w64-mingw32" ]]; then
-  ARCHIVE_NAME="trip-windows-x86.zip"
+  ARCHIVE_NAME="windows-x86.zip"
 elif [[ $HOST = "x86_64-w64-mingw32" ]]; then
-    ARCHIVE_NAME="trip-windows-x64.zip"
-elif [[ $HOST = "arm-linux-gnueabihf" ]]; then
-    ARCHIVE_NAME="trip-arm-x86.tar.gz"
-    ARCHIVE_CMD="tar -czf"
-elif [[ $HOST = "aarch64-linux-gnu" ]]; then
-    ARCHIVE_NAME="trip-arm-x64.tar.gz"
-    ARCHIVE_CMD="tar -czf"
+    ARCHIVE_NAME="windows-x64.zip"
 elif [[ $HOST = "x86_64-unknown-linux-gnu" ]]; then
-    ARCHIVE_NAME="trip-linux-x64.tar.gz"
+    if [[ $DEP_OPTS = "NO_WALLET=1" ]]; then    
+        ARCHIVE_NAME="linux-x64-no-wallet.tar.gz"
+        ARCHIVE_CMD="tar -czf"
+    else
+        ARCHIVE_NAME="linux-x64.tar.gz"
+        ARCHIVE_CMD="tar -czf"
+    fi  
+elif [[ $HOST = "i686-pc-linux-gnu" ]]; then
+    ARCHIVE_NAME="linux-x86.tar.gz"
     ARCHIVE_CMD="tar -czf"
 elif [[ $HOST = "x86_64-apple-darwin11" ]]; then
-    ARCHIVE_NAME="trip-osx-x64.zip"
+    ARCHIVE_NAME="osx-x64.zip"
 fi
 
 if [[ $HOST = "x86_64-apple-darwin11" ]]; then
-    find /home/travis/build/trip-project/trip/ -type f | grep -r -i *.dmg$ | xargs -i cp {} $OUTDIR/bin
+    find $TRAVIS_BUILD_DIR -type f | grep -i trip-qt.dmg$ | xargs -i cp {} $OUTDIR/bin
+    find $TRAVIS_BUILD_DIR -type f | grep -i trip-core.dmg$ | xargs -i cp {} $OUTDIR/bin
 else
     cp $TRAVIS_BUILD_DIR/src/qt/trip-qt $OUTDIR/bin/ || cp $TRAVIS_BUILD_DIR/src/qt/trip-qt.exe $OUTDIR/bin/ || echo "no QT Wallet"
     cp $TRAVIS_BUILD_DIR/src/tripd $OUTDIR/bin/ || cp $TRAVIS_BUILD_DIR/src/tripd.exe $OUTDIR/bin/
     cp $TRAVIS_BUILD_DIR/src/trip-cli $OUTDIR/bin/ || cp $TRAVIS_BUILD_DIR/src/trip-cli.exe $OUTDIR/bin/
     strip "$OUTDIR/bin"/* || echo "nothing to strip"
-    ls -lah $OUTDIR/bin
 fi
 
 cd $OUTDIR/bin
-ls -ah
 ARCHIVE_CMD="$ARCHIVE_CMD $ARCHIVE_NAME *"
 eval $ARCHIVE_CMD
 
