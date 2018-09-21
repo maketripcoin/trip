31b279bf1778943e8eb616225e2b1805deed1e83
maketripcoin <39806121+maketripcoin@users.noreply.github.com>
2018-09-21 01:02:02 +0100

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -55,12 +55,14 @@ static void convertSeed6(std::vector<CAddress>& vSeedsOut, const SeedSpec6* data
 static Checkpoints::MapCheckpoints mapCheckpoints =
     boost::assign::map_list_of
     (0, uint256("0x00000869497e2ba8571b447bc179e12c9157efd92fb587bfb9e1b4bb63db9eb1"))
-    (1361, uint256("0x94774ab4d12e1a87d99b231b901c4e92a9ee3e7bc8cee91c8e64aeee6a716d4a"));
+    (1361, uint256("0x94774ab4d12e1a87d99b231b901c4e92a9ee3e7bc8cee91c8e64aeee6a716d4a"))
+    (25001, uint256("0x82947925fb12ed1bcb35e5136b87422a03528ebf5b27df5bbaf54df274b0e26d"))
+    (25295, uint256("0x67f9b29ff3b79462abebc42a1e097cebb12d46ac7e1b03d9aa1e2c5d1c156401"));
 
 static const Checkpoints::CCheckpointData data = {
     &mapCheckpoints,
-    1535576284, // * UNIX timestamp of last checkpoint block
-    0,    // * total number of transactions between genesis and last checkpoint
+    1537487947, // * UNIX timestamp of last checkpoint block
+    50667,    // * total number of transactions between genesis and last checkpoint
                 //   (the tx=... number in the SetBestChain debug.log lines)
     2000        // * estimated number of transactions per day after checkpoint
 };
